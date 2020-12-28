5d3ef0b08c79ebdd8e7653abb3cb7c9ac5f04b63
Rain Babauta <rainbabauta1@gmail.com>
2020-12-28 12:42:08 -0800

    initial commit and created some base files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter } from "react-router-dom";
+import UserContext from './auth/UserContext.js';
+import Navigation from './routes-nav/Navigation.js';
+import Routes from './routes-nav/Routes.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <UserContext.Provider value={{currentUser, setCurrentUser}}>
+        <div className="App">
+          <Navigation />
+          <Routes />
+        </div>
+      </UserContext.Provider>
+    </BrowserRouter>
   );
 }
 
---
+++ src/country-codes/CountryCodes.js
@@ -0,0 +1,10 @@
+import React, { useContext } from "react";
+import UserContext from "../auth/UserContext";
+
+function CountryCodes() {
+  return (
+    <div>TEMP</div>
+  );
+}
+
+export default CountryCodes;
