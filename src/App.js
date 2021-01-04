bff10bf305a600e55e24a441cf9a2aba2e2e18b8
Rain Babauta <rainbabauta1@gmail.com>
2021-01-04 15:23:16 -0800

    Worked on tests for App and Navigation, had to add a file for user context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import React, { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import UserContext from './auth/UserContext.js';
 import Navigation from './routes-nav/Navigation.js';
 import Routes from './routes-nav/Routes.js';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
   return (
     <BrowserRouter>
       <UserContext.Provider value={{currentUser, setCurrentUser}}>
